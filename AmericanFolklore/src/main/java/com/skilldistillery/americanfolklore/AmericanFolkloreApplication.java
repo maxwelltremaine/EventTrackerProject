929381a5d08fe2e12fa89b00646937fc2e8f8c0f
Max <maxtremaine@aol.com>
2024-01-05 17:25:30 -0700

    Configure Spring REST project, connect and test

---
+++ AmericanFolklore/src/main/java/com/skilldistillery/americanfolklore/AmericanFolkloreApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.americanfolklore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmericanFolkloreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmericanFolkloreApplication.class, args);
+	}
+
+}
---
+++ AmericanFolklore/src/test/java/com/skilldistillery/americanfolklore/AmericanFolkloreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.americanfolklore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmericanFolkloreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
